4a44aaa3fe2afbcb5c6d18ee9ac75e2fa0d2765d
LEONARDOJORGE98 <56683773+LEONARDOJORGE98@users.noreply.github.com>
2022-12-02 10:58:12 -0300

    Add files via upload

---
+++ profiling.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from pandas_profiling import ProfileReport
+from pandas_profiling.utils.cache import cache_file
+
+#Data:
+#https://www.kaggle.com/datasets/whenamancodes/alcohol-effects-on-study?select=Maths.csv
+#https://www.kaggle.com/datasets/whenamancodes/alcohol-effects-on-study?select=Portuguese.csv
+#https://www.kaggle.com/datasets/sonukumari47/students-performance-in-exams
+#Student_Performance_new.csv
+#
+
+data_file = input('Digite o nome do arquivo: ')
+
+try:
+    df = pd.read_csv(f'data/{data_file}.csv', sep=',')
+except:
+    df = pd.read_csv(f'data/{data_file}.csv', sep=';')
+
+profile = ProfileReport(df, title=f"{data_file} Dataset")
+profile.to_file(f"{data_file}.html")
