a2bbcdcb2d7d20746fb0b3b03488418c6555d637
LEONARDOJORGE98 <56683773+LEONARDOJORGE98@users.noreply.github.com>
2022-12-13 18:43:29 -0300

    Add files via upload

---
+++ app_tabela.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode
+
+
+dados = pd.read_csv("data/Portuguese.csv")
+
+gb = GridOptionsBuilder.from_dataframe(dados)
+gb.configure_pagination(paginationAutoPageSize=True) #Add pagination
+gb.configure_side_bar() #Add a sidebar
+gb.configure_selection('multiple', use_checkbox=True, groupSelectsChildren="Group checkbox select children") #Enable multi-row selection
+gridOptions = gb.build()
+
+grid_response = AgGrid(
+    dados,
+    gridOptions=gridOptions,
+    data_return_mode='AS_INPUT', 
+    update_mode='MODEL_CHANGED', 
+    fit_columns_on_grid_load=False,
+    enable_enterprise_modules=True,
+    reload_data=True
+)
+
+data = grid_response['data']
+selected = grid_response['selected_rows'] 
+df = pd.DataFrame(selected) #Pass the selected rows to a new dataframe df
