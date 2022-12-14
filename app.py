4c4c8a9791674192dd69f0a842f852887b959449
Aline <alinesantanalimeira@gmail.com>
2022-12-13 21:47:41 -0300

    inclusao do sidebar sem ligacao com o dataset

--- app.py
+++ app.py
@@ -3,16 +3,18 @@ import pandas as pd
 import seaborn as seaborn
 import numpy as np
 import matplotlib.pyplot as plt
+#from sklearn import datasets 
 
 
+#informacoes no head da pagina
+st.set_page_config(page_title='Analise de dados', page_icon=':bar_chart')
 st.title("Análise de dados")
-st.write("Esta página tem como intuito mostrar a analise dos dados de arquivos csv")
+st.write("Esta página tem como objetivo mostrar a analise dos dados de arquivos csv")
 
 dados = pd.read_csv("dados/Portuguese.csv")
 
 
-
-st.dataframe(dados) #comando q aparece toda a tabela
+st.dataframe(dados)  # comando q aparece toda a tabela
 
 # grafico fem X masculino
 genero_fem = dados.age.loc[dados.sex == 'F'].value_counts()
@@ -31,3 +33,22 @@ plt.title("Feminino vs Masculino")
 plt.show()
 
 st.pyplot(plt)
+
+
+#outros testes de como utilizar o streamlit no fim da pagina
+dataset_name = st.sidebar.selectbox("Selected dataset", ("Maths", "Portuguese"))
+
+classifier_name = st.sidebar.selectbox("Selected classifier", ("KNN", "SVM","Random forest"))
+
+def get_dataset(dataset_name):
+    if dataset_name == "Maths":
+        data = dataset.load_maths()
+    else:
+        data = dataset.load_portuguese()
+    X = data.data
+    y = data.target
+    return X, y
+
+X, y = get_dataset(dataset_name)
+st.write("shape of dataset", X.shape)
+st.write("number of classes", len(np.unique(y)))
--- app_tabela.py
+++ app_tabela.py
@@ -4,24 +4,25 @@ import numpy as np
 from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode
 
 
-dados = pd.read_csv("data/Portuguese.csv")
+dados = pd.read_csv("dados/Portuguese.csv")
 
 gb = GridOptionsBuilder.from_dataframe(dados)
-gb.configure_pagination(paginationAutoPageSize=True) #Add pagination
-gb.configure_side_bar() #Add a sidebar
-gb.configure_selection('multiple', use_checkbox=True, groupSelectsChildren="Group checkbox select children") #Enable multi-row selection
+gb.configure_pagination(paginationAutoPageSize=True)  # Add pagination
+gb.configure_side_bar()  # Add a sidebar
+gb.configure_selection('multiple', use_checkbox=True,
+                       groupSelectsChildren="Group checkbox select children")  # Enable multi-row selection
 gridOptions = gb.build()
 
 grid_response = AgGrid(
     dados,
     gridOptions=gridOptions,
-    data_return_mode='AS_INPUT', 
-    update_mode='MODEL_CHANGED', 
+    data_return_mode='AS_INPUT',
+    update_mode='MODEL_CHANGED',
     fit_columns_on_grid_load=False,
     enable_enterprise_modules=True,
     reload_data=True
 )
 
 data = grid_response['data']
-selected = grid_response['selected_rows'] 
-df = pd.DataFrame(selected) #Pass the selected rows to a new dataframe df
+selected = grid_response['selected_rows']
+df = pd.DataFrame(selected)  # Pass the selected rows to a new dataframe df
